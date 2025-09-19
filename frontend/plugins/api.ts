b4f09b828cc84f88fd3ee17bf91431c7824eda1f
Le Nguyen <>
2025-09-19 17:41:24 +0700

    feat: update fetch plugin

--- frontend/plugins/api.ts
+++ frontend/plugins/api.ts
@@ -2,5 +2,5 @@ import { Api } from '~/api'
 
 export default defineNuxtPlugin(({ provide }) => {
   const { $fetchClient } = useNuxtApp()
-  provide('api', new Api())
+  provide('api', new Api($fetchClient))
 })
